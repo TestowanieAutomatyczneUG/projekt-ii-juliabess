da32ecfe2e3631f7bb4c63d7fa795227aeeef443
Julia Bessman <julia.bessman@autonomik.pl>
2022-01-29 00:46:52 +0100

    function test_rejestracja

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,3 +13,9 @@ def create_request_mock(to_mock, fake_response):
 class ZalogujSieTest(unittest.TestCase):
     def setUp(self) -> None:
         self.app = ZalogujSie()
+
+    @patch('src.application.requests.post')
+    def test_rejestracja(self, mock_post):
+        create_request_mock(mock_post, FakeResponse(201, {'id': 1}))
+        response = self.app.rejestracja('91230303040', 'testowanie', 'haslo1234')
+        assert_that(response.json['id']).is_greater_than(0)
