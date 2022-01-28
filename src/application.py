3b00b167290c065d7c53c8add59a9164f31e7b74
Julia Bessman <julia.bessman@autonomik.pl>
2022-01-29 00:43:20 +0100

    add class ZalogujSie

---
+++ src/application.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class ZalogujSie:
+
+
+    def __init__(self):
+
+        self.add = "https://kocham.testowanie/add"
+        self.update = "https://kocham.testowanie/update"
+        self.delete = "https://kocham.testowanie/delete"
+        self.register = "https://kocham.testowanie/register"
+        self.loginn = "https://kocham.testowanie/login"
+        self.get = "https://kocham.testowanie/users"
+        self.getOne = "https://kocham.testowanie/{}"
