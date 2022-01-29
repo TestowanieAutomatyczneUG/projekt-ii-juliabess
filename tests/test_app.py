214fe7ffc8f622219bedb0f8346859c343e9292f
Julia Bessman <julia.bessman@autonomik.pl>
2022-01-29 01:11:00 +0100

    test_login_haslo_float

--- tests/test_app.py
+++ tests/test_app.py
@@ -94,3 +94,7 @@ class ZalogujSieTest(unittest.TestCase):
     def test_login_haslo_int(self):
         assert_that(self.app.login).raises(
             ValueError).when_called_with('login', 1)
+
+    def test_login_haslo_float(self):
+        assert_that(self.app.login).raises(
+            ValueError).when_called_with('login', 1.4)
