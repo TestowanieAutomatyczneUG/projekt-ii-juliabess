0df713110422b31c8ab51c7122872fa345e9b897
Julia Bessman <julia.bessman@autonomik.pl>
2022-01-29 01:07:20 +0100

    function rejestracja

--- src/application.py
+++ src/application.py
@@ -13,3 +13,26 @@ class ZalogujSie:
         self.loginn = "https://kocham.testowanie/login"
         self.get = "https://kocham.testowanie/users"
         self.getOne = "https://kocham.testowanie/{}"
+
+    def rejestracja(self, pesel, login, haslo):
+        if type(pesel) is not str:
+            raise ValueError
+        if len(pesel) != 11:
+            raise ValueError
+        if type(login) is not str:
+            raise ValueError
+        if len(login) <= 0:
+            raise ValueError
+        if type(haslo) is not str:
+            raise ValueError
+        if len(haslo) <= 0:
+            raise ValueError
+
+        body = {'pesel': pesel, 'login': login, 'haslo':haslo}
+        response = requests.post(self.register , json=body)
+        if 200 <= response.status_code <= 299:
+            return response
+        elif response.status_code == 409:
+            return 'Podany użytkownik już istnieje'
+        else:
+            return 'Ups... Coś poszło nie tak'
